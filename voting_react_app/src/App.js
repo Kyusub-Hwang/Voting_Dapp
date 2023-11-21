c7898534b9870d0462e7587e475e7b6820ac1bbb
Kyusub-Hwang <kyusub.hwang.dev@gmail.com>
2023-11-21 15:50:37 -0500

    debugging 0.1

--- voting_react_app/src/App.js
+++ voting_react_app/src/App.js
@@ -4,12 +4,135 @@ import {useState, useEffect} from 'react';
 import {ethers} from 'ethers';
 import {contractAbi, contractAddress} from './Constant/constant';
 import Login from './Components/Login';
+import Connected from './Components/Connected';
 
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [votingStatus, setVotingStatus] = useState(false);
+  const [remainingTime, setremainingTime] = useState('');
+  const [candidates, setCandidates] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [canVote, setCanVote] = useState(true);
+
+  useEffect(() => {
+    getCandidates();
+    getRemainingTime();
+    getCurrentStatus();
+
+    if (window.ethereum){
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+    }
+
+    return () => {
+      if (window.ethereum){
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+      }
+    }
+  },[]);
+
+  const handleAccountsChanged = (accounts) => {
+    if (accounts.length > 0 && account !== accounts[0] ){
+      setAccount(accounts[0]);
+    } else {
+      setIsConnected(false);
+      setAccount(null);
+    }
+  }
+
+  const vote = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const tx = await contractInstance.vote(number);
+    await tx.wait();
+    canVote();
+  }
+
+  const getVoteStatus = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const voteStatus = await contractInstance.voters(await signer.getAddress());
+    setCanVote(voteStatus);
+  }
+
+  const getCandidates = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const candidatesList = await contractInstance.getAllVotesOfCandiates();
+    const formattedCandidates = candidatesList.map((candidate, index)=>{
+      return {
+        index,
+        name: candidate.name,
+        voteCount: candidate.voteCount.toNumber()
+      }
+    })
+    setCandidates(formattedCandidates);
+  }
+
+  const getCurrentStatus = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const status = await contractInstance.getVotingStatus();
+    setVotingStatus(status);
+  }
+
+  const getRemainingTime = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const time = await contractInstance.getRemainingTime();
+    setremainingTime(parseInt(time,16));
+  }
+
+  const connectToMetamask = async () => {
+    if(window.ethereum){
+      try{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        console.log("Metamask connected :" + address);
+
+        setIsConnected(true);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.error("Metamask undetected");
+    }
+  }
+
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value);
+    console.log(number)
+  }
+
   return (
     <div className="App">
-      <Login />
+      {isConnected ? (<Connected account={account} candidates={candidates} remainingTime={remainingTime} voteFunction={vote}/>) : (<Login connectWallet={connectToMetamask} />) };
     </div>
   );
 }
---
+++ voting_react_app/src/Components/Connected.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Connected = (props) => {
+    return (
+        <div className='login-container'>
+            <h1 className='connected-header'>You are conntected to Metamask</h1>
+            <h1 className='connected-account'>Metamask account : {props.account}</h1>
+            <h1 className='connected-account'>Remaining Time : {props.remainingTime}</h1>
+
+            <div>
+                <input type="number" placeholder='Enter Candidate number' value={props.number} onChange={props.handleNumberChange}></input>
+                <button className='login-button' onClick={props.voteFunction}>Vote</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Candidate Name</th>
+                        <th>Candidate Votes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.candidates.map((candidate, index)=>(
+                        <tr key={index}>
+                            <td>{candidate.index}</td>
+                            <td>{candidate.name}</td>
+                            <td>{candidate.voteCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Connected;
